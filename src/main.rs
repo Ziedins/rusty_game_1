5453081e7dee1c06dcde73dc9285d5ddacd762f5
Reinis Ziedins <reinis.z@legalbet.com>
2023-03-20 22:32:08 +0700

    Rusty tutorial step 1: Add sprite, background music, game logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use rusty_engine::prelude::*;
+
+struct GameState {
+    health: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    let sprite = game.add_sprite("player", SpritePreset::RacingCarGreen);
+    sprite.scale = 1.9;
+
+    game.audio_manager.play_music(MusicPreset::MysteriousMagic, 1.0);
+
+    game.add_logic(game_logic);
+
+    game.run(GameState {health: 100});
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    let player = engine.sprites.get_mut("player").unwrap();
+
+    player.rotation += std::f32::consts::PI * engine.delta_f32;
+
+    if player.translation.x > 100.0 {
+        game_state.health -=1;
+    }
+
 }
